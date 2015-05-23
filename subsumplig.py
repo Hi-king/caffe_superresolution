d36a8bb1bcbecaa9b281de052ed282afc6b44925
hiking <hikingko1@gmail.com>
2015-05-23 15:13:51 +0900

    initial commit

---
+++ subsumplig.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import cv2
+import argparse
+import os
+import scipy
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("input_dir")
+parser.add_argument("resized_dir")
+parser.add_argument("output_dir")
+args = parser.parse_args()
+
+
+patch_size = (20, 20) #偶数であること
+if patch_size[0] % 2 == 1 or patch_size[1] % 2 == 1:
+    print("patch_size should be even")
+    exit(1)    
+
+for filename in os.listdir(args.input_dir):
+    img = cv2.imread(args.input_dir+"/"+filename)
+    img = cv2.cvtColor(img, cv2.cv.CV_BGR2GRAY)
+    if img.shape == (100, 130):
+        first_resized = cv2.resize(img, (20, 26))
+        resized = cv2.resize(first_resized, (130, 100))
+        cv2.imwrite(args.resized_dir+"/"+filename+".png", resized)
+        
+
+        # 折り返し画像
+        large_width = scipy.concatenate((scipy.fliplr(resized), resized, scipy.fliplr(resized)), axis=1)
+        large = scipy.concatenate((scipy.flipud(large_width), large_width, scipy.flipud(large_width)), axis=0)
+
+        h, w = resized.shape
+        for original_x in xrange(w):
+            for original_y in xrange(h):
+                x, y = original_x + w, original_y + h
+                subimg = large[y-patch_size[1]/2:y+patch_size[1]/2, x-patch_size[0]/2:x+patch_size[0]/2]
+                #cv2.imshow("test", img)
+                #cv2.waitKey(-1)
+                target_filename="{}_{}_{}.png".format(filename, original_x, original_y)
+                cv2.imwrite(args.output_dir+"/"+target_filename, subimg)
+                print("{} {}".format(target_filename, img[original_y, original_x]))
